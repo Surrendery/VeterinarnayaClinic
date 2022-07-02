d9235e84f9016ee4e44b125dc100f7f92060d4a1
pukse <puk.serka@gmail.com>
2022-07-02 20:20:27 +0400

    final final fix

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,38 +12,38 @@ public class App {
         AppStart.start();
     }
     public static class AppStart {
-        private static final PatientServiceImpl patientService = new PatientServiceImpl();
-        private static final DoctorServiceImpl doctorService = new DoctorServiceImpl();
-        private static final AppointmentServiceImpl appointmentService = new AppointmentServiceImpl();
+        private static final PatientServiceRlztn patientService = new PatientServiceRlztn();
+        private static final DoctorServiceRlztn doctorService = new DoctorServiceRlztn();
+        private static final AppointmentServiceRlztn appointmentService = new AppointmentServiceRlztn();
 
         public static void start() {
             Scanner scanner = new Scanner(System.in);
-            if (Authentication.auth()) while (true) {
+            if (Authentication.authentication()) while (true) {
                 System.out.print("""
-                    0 - Escape
-                    1 - Create patient
-                    2 - Create doctor
-                    3 - Create appointment
-                    4 - Delete patient
-                    5 - Show all patients
-                    6 - Edit patients fio
-                    7 - Show all patient's appointments
-                    8 - Change appointment status
+                    0 - выйти
+                    1 - создать пациента
+                    2 - удалить пациента
+                    3 - создать доктора
+                    4 - создать приём
+                    5 - показать всех пациентов
+                    6 - изменить фио пациента
+                    7 - показать все приёмы пациента
+                    8 - изменить статус приёма
                     """);
                 switch (scanner.nextInt()) {
                     case 0 -> System.exit(0);
-                    case 1 -> PatientServiceImpl.createPatient();
-                    case 2 -> DoctorServiceImpl.createDoctor();
-                    case 3 -> appointmentService.createAppointment();
-                    case 4 -> PatientServiceImpl.deleteUser();
-                    case 5 -> PatientServiceImpl.showAllPatients();
-                    case 6 -> PatientServiceImpl.editPatientFio();
-                    case 7 -> AppointmentServiceImpl.showAllPatientAppointments();
-                    case 8 -> AppointmentServiceImpl.changeAppointmentStatus();
+                    case 1 -> PatientServiceRlztn.createPatient();
+                    case 2 -> PatientServiceRlztn.deletePatient();
+                    case 3 -> DoctorServiceRlztn.createDoctor();
+                    case 4 -> appointmentService.createAppointment();
+                    case 5 -> PatientServiceRlztn.showAllPatients();
+                    case 6 -> PatientServiceRlztn.editPatientFcs();
+                    case 7 -> AppointmentServiceRlztn.showAllPatientAppointments();
+                    case 8 -> AppointmentServiceRlztn.changeAppointmentStatus();
                 }
             }
             else {
-                System.out.println("Incorrect!");
+                System.out.println("Неправильный логин или пароль!");
             }
         }
     }
@@ -56,14 +56,46 @@ public class App {
         public static final String LOGIN = "login";
         public static final String PASSWORD = "password";
 
-        public static boolean auth() {
+        public static boolean authentication() {
             Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter login: ");
+            System.out.print("Введите логин: ");
             String login = scanner.nextLine();
-            System.out.print("Enter password: ");
+            System.out.print("Введите пароль: ");
             String password = scanner.nextLine();
 
             return login.equals(LOGIN) & password.equals(PASSWORD);
+        }
+    }
+    @Getter
+    @Setter
+    public static class Patient {
+
+        private UUID id = UUID.randomUUID();
+
+        private String fcs;
+
+        private Calendar registrationDate;
+
+        public String getRegistrationDate(){
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            return sdf.format(registrationDate.getTime());
+        }
+
+        public Patient(String fcs, Calendar registrationDate) {
+
+            this.fcs = fcs;
+            this.registrationDate = registrationDate;
+        }
+    }
+    @Getter
+    @Setter
+    public static class Doctor {
+        private UUID id = UUID.randomUUID();
+
+        private String fcs;
+
+        public Doctor(String fcs) {
+            this.fcs = fcs;
         }
     }
     @Getter
@@ -72,7 +104,7 @@ public class App {
 
         private UUID id = UUID.randomUUID();
 
-        private String patientFio;
+        private String patientFcs;
         private Calendar registrationDate;
 
         private AppointmentStatus status;
@@ -81,31 +113,98 @@ public class App {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
             return sdf.format(registrationDate.getTime());
         }
-        public Appointment(String patientFio, Calendar registrationDate,AppointmentStatus status){
+        public Appointment(String patientFcs, Calendar registrationDate,AppointmentStatus status){
 
-            this.patientFio = patientFio;
+            this.patientFcs = patientFcs;
             this.registrationDate = registrationDate;
             this.status = status;
         }
     }
-    public static class AppointmentServiceImpl {
+    public static class PatientServiceRlztn {
+
+
+
+        public static void createPatient() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Введите фио: ");
+            String fcs = scanner.nextLine();
+            System.out.print("Введите год регистрации: ");
+            int year = scanner.nextInt();
+            System.out.print("Введите месяц регистрации: ");
+            int month = scanner.nextInt();
+            System.out.print("Введите день регистрации: ");
+            int day = scanner.nextInt();
+            Calendar registrationDate = new GregorianCalendar(year, month, day);
+
+            Storage.patients.add(new Patient(fcs, registrationDate));
+            System.out.println("Пациент был добавлен!");
+        }
+
+        public static void showAllPatients() {
+            if (Storage.patients.isEmpty()) {
+                System.out.println("Пациентов нет");
+                return;
+            }
+
+            Storage.patients.forEach(p -> {
+                System.out.printf("ID: %s\nфио: %s\nДата регистрации: %s", p.getId(), p.getFcs(), p.getRegistrationDate());
+                System.out.print("\n");
+            });
+        }
+
+        public static void deletePatient() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Введите ID пациента: ");
+            UUID patientId = UUID.fromString(scanner.nextLine());
+            Storage.patients.removeIf(p -> p.getId().equals(patientId));
+
+            System.out.println("Пациент был удалён!");
+        }
+
+        public static void editPatientFcs() {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Введите ID пользователя: ");
+            UUID patientId = UUID.fromString(scanner.nextLine());
+            Patient patient = Storage.patients.stream().filter(p -> p.getId().equals(patientId)).findFirst().get();
+
+
+            System.out.print("Введите фио: ");
+            String fcs = scanner.nextLine();
+
+            patient.setFcs(fcs);
+        }
+    }
+    public static class DoctorServiceRlztn {
+
+
+        public static void createDoctor() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Введите фио: ");
+            String fcs = scanner.nextLine();
+
+            Storage.doctors.add(new Doctor(fcs));
+            System.out.println("Доктор " + fcs + " был добавлен!");
+        }
+    }
+    public static class AppointmentServiceRlztn {
 
 
         public void createAppointment() {
 
             Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Enter patient fio: ");
-            String patientFio = scanner.nextLine();
-            System.out.print("Enter reg year: ");
+            System.out.print("Введите фио пациента: ");
+            String patientFcs = scanner.nextLine();
+            System.out.print("Введите год регистрации: ");
             int year = scanner.nextInt();
-            System.out.print("Enter reg month: ");
+            System.out.print("Введите месяц регистрации: ");
             int month = scanner.nextInt();
-            System.out.print("Enter reg day: ");
+            System.out.print("Введите день регистрации: ");
             int day = scanner.nextInt();
             Calendar registrationDate = new GregorianCalendar(year, month, day);
 
-            Appointment appointment = new Appointment(patientFio, registrationDate, AppointmentStatus.NEW);
+            Appointment appointment = new Appointment(patientFcs, registrationDate, AppointmentStatus.NEW);
 
             Storage.appointments.add(appointment);
 
@@ -114,17 +213,17 @@ public class App {
         public static void showAllPatientAppointments() {
             Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Enter patient fio: ");
-            String patientFio = scanner.nextLine();
+            System.out.print("Введите фио пациента: ");
+            String patientFcs = scanner.nextLine();
 
-            if (Storage.appointments.stream().noneMatch(a -> a.getPatientFio().equals(patientFio))) {
-                System.out.println("This patient has no appointments");
+            if (Storage.appointments.stream().noneMatch(a -> a.getPatientFcs().equals(patientFcs))) {
+                System.out.println("У этого пациента нет приёмов");
                 return;
             }
 
-            System.out.println("List of appointments for " + patientFio);
+            System.out.println("Список приёмов для " + patientFcs);
             Storage.appointments.forEach(a -> {
-                if (a.getPatientFio().equals(patientFio))
+                if (a.getPatientFcs().equals(patientFcs))
                     System.out.println(a.getRegistrationDate() + " " + a.getStatus().toString());
             });
 
@@ -133,16 +232,16 @@ public class App {
         public static void changeAppointmentStatus() {
             Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Enter appointment id: ");
+            System.out.print("Введите ID приёма: ");
             UUID appointmentId = UUID.fromString(scanner.nextLine());
 
             System.out.print("""
-                Enter new status:
-                0 - NEW
-                1 - IN PROCESS
-                2 - CANCELLED
-                3 - AWAITING PAYMENT
-                4 - COMPLETED
+                Выберите новый статус:
+                0 - Новый
+                1 - В процессе
+                2 - Отменён
+                3 - Ожидается оплата
+                4 - Выполнен
                 """);
 
             int statusCode = scanner.nextInt();
@@ -160,79 +259,12 @@ public class App {
 
         }
     }
-    public static class DoctorServiceImpl {
-
-
-        public static void createDoctor() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter fio: ");
-            String fio = scanner.nextLine();
-
-            Storage.doctors.add(new Doctor(fio));
-            System.out.println("Dr " + fio + " was added!");
-        }
-    }
-    public static class PatientServiceImpl {
-
-
-
-        public static void createPatient() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter fio: ");
-            String fio = scanner.nextLine();
-            System.out.print("Enter reg year: ");
-            int year = scanner.nextInt();
-            System.out.print("Enter reg month: ");
-            int month = scanner.nextInt();
-            System.out.print("Enter reg day: ");
-            int day = scanner.nextInt();
-            Calendar registrationDate = new GregorianCalendar(year, month, day);
-
-            Storage.patients.add(new Patient(fio, registrationDate));
-            System.out.println("Patient was added!");
-        }
-
-        public static void showAllPatients() {
-            if (Storage.patients.isEmpty()) {
-                System.out.println("No patients");
-                return;
-            }
-
-            Storage.patients.forEach(p -> {
-                System.out.printf("ID: %s\nFIO: %s\nRegistration date: %s", p.getId(), p.getFio(), p.getRegistrationDate());
-                System.out.println("\n");
-            });
-        }
-
-        public static void deleteUser() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter user id: ");
-            UUID patientId = UUID.fromString(scanner.nextLine());
-            Storage.patients.removeIf(p -> p.getId().equals(patientId));
-
-            System.out.println("User was delete!");
-        }
-
-        public static void editPatientFio() {
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.print("Enter user id: ");
-            UUID patientId = UUID.fromString(scanner.nextLine());
-            Patient patient = Storage.patients.stream().filter(p -> p.getId().equals(patientId)).findFirst().get();
-
-
-            System.out.print("Enter fio: ");
-            String fio = scanner.nextLine();
-
-            patient.setFio(fio);
-        }
-    }
     public enum AppointmentStatus {
-        NEW("NEW"),
-        IN_PROCESS("IN-PROCESS"),
-        CANCELLED("CANCELLED"),
-        AWAITING_PAYMENT("AWAITING-PAYMENT"),
-        COMPLETED("COMPLETED");
+        NEW("Новый"),
+        IN_PROCESS("В процессе"),
+        CANCELLED("Отменён"),
+        AWAITING_PAYMENT("Ожидается оплата"),
+        COMPLETED("Выполнен");
 
         private final String text;
 
@@ -243,39 +275,6 @@ public class App {
         @Override
         public String toString() {
             return text;
-        }
-    }
-
-    @Getter
-    @Setter
-    public static class Doctor {
-        private UUID id = UUID.randomUUID();
-
-        private String fio;
-
-        public Doctor(String fio) {
-            this.fio = fio;
-        }
-    }
-    @Getter
-    @Setter
-    public static class Patient {
-
-        private UUID id = UUID.randomUUID();
-
-        private String fio;
-
-        private Calendar registrationDate;
-
-        public String getRegistrationDate(){
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            return sdf.format(registrationDate.getTime());
-        }
-
-        public Patient(String fio, Calendar registrationDate) {
-
-            this.fio = fio;
-            this.registrationDate = registrationDate;
         }
     }
 
